b9c28ad2cb88c7bfda6bcf4d2ac98e28c7d44c89
Paulo <paulohenriquesms10@gmail.com>
2022-05-10 01:25:29 -0300

    módulo de react funcional

---
+++ react-funcional/src/UseState.js
@@ -0,0 +1,36 @@
+import React, { useState} from 'react';
+
+function UseState() {
+
+  const [ numero, setNumero ] = useState()
+  const [ segundoNumero, setSegundoNumero ] = useState()
+  const [ resultado, setResultado ] = useState();
+
+  const somar = () => {
+    const numeroInt = parseInt(numero)
+    const segNumeroInt = parseInt(segundoNumero)
+
+    setResultado( numeroInt + segNumeroInt )
+  }
+
+  return (
+    <div>
+      Número 1:<br />
+      <input type="text" value={numero} 
+              onChange={e => setNumero(e.target.value)}/><br/>
+
+      Número 2:<br />
+      <input type="text" value={segundoNumero} 
+              onChange={e => setSegundoNumero(e.target.value)}/><br/>
+
+      <button onClick={somar}>Somar</button> <br/>
+
+
+
+      Resultado:<br />
+      <input type="text" value={resultado} onChange={e => setResultado(e.target.value)} /><br/>
+    </div>
+  );
+}
+
+export default UseState;
